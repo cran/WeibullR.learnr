4ff1d139644a1fa9a5311cf309bf289b3b9b222d
Paul Govan <pgovan1@aggienetwork.com>
2023-06-06 06:50:06 +0000

    version 0.1.1

---
+++ R/WeibullR.learnr.R
@@ -0,0 +1,15 @@
+#' An Interactive Introduction to Life Data Analysis
+#'
+#' WeibullR.learnr is an interactive introduction to Life Data Analysis.
+#' @import learnr
+#' @import WeibullR
+#' @export
+#' @seealso \url{https://paulgovan.github.io/WeibullR.learnr/}
+#' @return This function does not return a value.
+#' @examples
+#' if (interactive()) {
+#'   WeibullR.learnr()
+#' }
+WeibullR.learnr <- function() {
+  learnr::run_tutorial('learnr', package = 'WeibullR.learnr')
+}
